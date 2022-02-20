fa6afa727b45c4f2cfe3f3852021ded968b32b5d
Viachaslau <viachaslaunasyrau@gmail.com>
2022-02-21 00:09:54 +0300

    fix: fix function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if(n<0){
+        let num = Math.abs(n);
+        return Number(num.toString().split('').reverse().join(''))
+    }
+    return Number(n.toString().split('').reverse().join(''))
 }
